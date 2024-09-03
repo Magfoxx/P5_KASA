983dfb8f05200977d716395556cd7bc89af83725
Magfoxx <hamcha.martial@gmail.com>
2024-09-03 16:49:06 +0200

    Ajout de quelques tags dans index.html et correction orthographe de la page About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -26,7 +26,7 @@ function About() {
             <p>La qualité du service est au coeur de notre engagement chez Kasa. Nous veillons à ce que chaque intéraction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance.</p>
           </Collapse>
           <Collapse title="Sécurité">
-            <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaques logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p>
+            <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p>
           </Collapse>
         </div>
       </main>
